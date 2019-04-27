7cc24bf7ed7b977fc3d2ac44b44f53074c06b0a8
Anthony <anthony.cersosimo@gmail.com>
2019-04-27 16:16:14 -0400

    replaced the value attributes on radio buttons to a, b, c, d and tweaked the time jvascript to attempt to stop at 0, hasnt worked so far

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,16 @@
 var answerCorrect = 0;
 var answerWrong = 12 - answerCorrect;
 
+//Array of correct answers :)
+var answerKey = ["q1b", "q2d", "q3c", "q4a", "q5c", "q6a", "q7b", "q8d", "q9d", "q10a", "q11a", "q12b"]
 
 window.onload = function() {
+  // Hide the Quiz Body on the page load
   $(".quiz-body").hide();
+
+  // Evaluate 
+
+  // Events to execute on begin quiz button
   $("#start").on("click", function(){
     start();
     $("#display").text("12 Minutes");
@@ -11,57 +18,61 @@ window.onload = function() {
   });
 };
 
-  // Start Timer
-  var intervalId;
-  
-  var clockRunning = false;
-  var time = 720;
-  
-  function reset() {
-  
-    time = 720;
+// Start Timer
+var intervalId;
 
+var clockRunning = false;
+var time = 720;
+
+function reset() {
+
+  time = 720;
+
+
+  $("#display").text("12:00");
+}
+function start() {
   
-    $("#display").text("12:00");
+  if (!clockRunning) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
   }
-  function start() {
-    
-    if (!clockRunning) {
-      intervalId = setInterval(count, 1000);
-      clockRunning = true;
-    }
+}
+function stop() {
+
+  clearInterval(intervalId);
+  clockRunning = false;
+}
+
+function count() {
+
+  time--;
+
+  var converted = timeConverter(time);
+  console.log(converted);
+
+  $("#display").text(converted);
+}
+function timeConverter(t) {
+
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
   }
-  function stop() {
-  
-    clearInterval(intervalId);
-    clockRunning = false;
+
+  if (minutes === 0) {
+    minutes = "00";
   }
-  
-  function count() {
-  
-    time--;
-  
-    var converted = timeConverter(time);
-    console.log(converted);
-  
-    $("#display").text(converted);
+
+  if (seconds === 0) {
+    stop();
   }
-  function timeConverter(t) {
-  
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-  
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-  
-    if (minutes === 0) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "00" + minutes;
-    }
-  
-    return minutes + ":" + seconds;
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
   }
-  // End Timer
+
+  return minutes + ":" + seconds;
+}
+// End Timer
